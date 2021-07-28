8307afee7e1e9692ac9842f1cf18d3621f26a773
GoldenGuts <jyotirmaygoldsmith@gmail.com>
2021-07-29 00:40:51 +0530

    updated for https

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const express = require('express')
-var exphbs  = require('express-handlebars');
+const exphbs  = require('express-handlebars');
 const https = require('https');
 const fs = require('fs');
 const path = require('path')
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 const server = require('http').createServer(app);
-const io = require("socket.io")(server);
+
 const httpsServer = https.createServer({
   key: fs.readFileSync('./key.pem'),
   cert: fs.readFileSync('./cert.pem'),
 }, app);
+
+//For running on Website
+const io = require("socket.io")(httpsServer);
+
+//For running local
+// const io = require("socket.io")(server);
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -22,10 +29,13 @@ app.use('/', require('./routes/base.js'))
 app.use('/alerts', require('./routes/alert.js'))
 app.use('/', require('./routes/webhook'))
 
-// httpsServer.listen(port, () => {
-//   console.log('HTTPS Server running on port 3000');
-// });
 
-server.listen(port, () => {
-  console.log(`listening on *:${port}`);
+//HTTPS for the website
+httpsServer.listen(port, () => {
+  console.log('HTTPS Server running on port 3000');
 });
+
+// HTTP for the loca env
+// server.listen(4000, () => {
+//   console.log(`listening on *:${4000}`);
+// });
