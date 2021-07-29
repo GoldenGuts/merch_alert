97aa14e05d8151725f31c435bd294cea174c640d
GoldenGuts <jyotirmaygoldsmith@gmail.com>
2021-07-30 02:12:15 +0530

    public/js/generate_links:

--- public/js/generate_links.js
+++ public/js/generate_links.js
@@ -1,21 +1,22 @@
-async function generateLink() {
-	const search_data = document.getElementById("search_box").value
-	console.log("inside generate links");
-	const response = fetch('https://forthefans.in:3000/check-streamer', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'text/plain',
-		},
-		body: search_data
-	})
-		.then(res => res.json())
-		.then(jsonResponse => {
-			console.log(jsonResponse);
-		})
+const generateLink = async () => {
+	const search_data = document.getElementById("search_box").value	
+	const rawResponse = await fetch('https://forthefans.in:3000/check-streamer', {
+	  method: 'POST',
+	  headers: {
+	    'Accept': 'application/json',
+	    'Content-Type': 'text/plain'
+	  },
+	  body: search_data
+	});
+	const content = await rawResponse.json();
 
-	document.getElementById("generated_url_block").style.display = "block";
-	document.getElementById("generate").innerHTML = "DO NOT SHARE THIS URL";
-	const link = search_data + (Math.floor(Math.random() * 10000) + 111);
-	document.getElementById("generated_url").value = '';
-	document.getElementById("generated_url").value = "https://forthefans.in:3000/alerts/"+link;
-}
+	if(content == null) {
+		document.getElementById("generated_url_block").style.display = "block";
+		document.getElementById("generate").innerHTML = "DO NOT SHARE THIS URL";
+		const link = search_data + (Math.floor(Math.random() * 10000) + 111);
+		document.getElementById("generated_url").value = '';
+		document.getElementById("generated_url").value = "https://forthefans.in:3000/alerts/"+link;	
+	} else {
+		console.log(content);
+	}
+};
