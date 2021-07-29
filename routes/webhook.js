af3eed48dd004639a984d9541adfab2853cbd426
GoldenGuts <jyotirmaygoldsmith@gmail.com>
2021-07-29 12:37:24 +0530

    webhook updated

--- routes/webhook.js
+++ routes/webhook.js
@@ -4,7 +4,10 @@ const router = express.Router()
 router.post('/get_data', (request,response) => {
 	//code to perform particular action.
 	//To access POST variable use req.body()methods.
-	console.log(request.body);
+	response.set('Content-Type', 'text/plain')
+	console.log('this ran');
+	response.status(200).json({ message: 'ok' });
+	console.log(response.body);
 	// console.log(response);
 });
 
