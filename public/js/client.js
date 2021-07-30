0c765643edc21987dd50be988315649a4c0fea18
GoldenGuts <jyotirmaygoldsmith@gmail.com>
2021-07-30 21:32:09 +0530

    public/js:

--- public/js/client.js
+++ public/js/client.js
@@ -1,9 +1,20 @@
-// const socket = io("https://forthefans.in:3000/alerts"); 
+const socket = io("https://forthefans.in:3000/alerts");
 
-// socket.on('message', message => {
-// 	console.log(message);
-// })
+if ( window.location.href == "https://forthefans.in:3000/" ){
 
-// socket.on("connect", () => {
-// 	socket.emit("streamer_url", window.location.href);
-// });
+	const test_button = document.getElementById('test_alert');
+
+	test_button.addEventListener('click', event => {
+		socket.emit("test_alert", "message");
+	});
+
+} else {
+	socket.on('message', message => {
+		console.log(message);
+	})
+
+	socket.on("connect", () => {
+		socket.emit("streamer_url", window.location.href);
+	});
+
+}
--- public/js/test_alert.js
+++
@@ -1,6 +0,0 @@
-const socket = io("https://forthefans.in:3000"); 
-const test_button = document.getElementById('test_alert');
-
-test_button.addEventListener('click', event => {
-	socket_alert.emit("test_alert", "message");
-});
