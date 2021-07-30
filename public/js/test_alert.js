71ab32b6779f1f1171fdd3b00ce1efecfc1820d5
GoldenGuts <jyotirmaygoldsmith@gmail.com>
2021-07-30 21:25:09 +0530

    test

--- public/js/test_alert.js
+++ public/js/test_alert.js
@@ -1,4 +1,4 @@
-const socket = io("https://forthefans.in:3000/"); 
+const socket = io("https://forthefans.in:3000"); 
 const test_button = document.getElementById('test_alert');
 
 test_button.addEventListener('click', event => {
