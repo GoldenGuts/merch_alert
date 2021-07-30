e65cedd9f6443fd2ca4c8a1e43bd88de2979f322
GoldenGuts <jyotirmaygoldsmith@gmail.com>
2021-07-30 23:37:03 +0530

    routes/alert:

--- routes/alert.js
+++ routes/alert.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const { mysql, db } = require('../mysql_config/config')
 const router = express.Router();
+let blankPage = 1;
 
 router.get('/:streamer_url', (req, res)=>{
 
@@ -21,21 +22,22 @@ router.get('/:streamer_url', (req, res)=>{
 		}
 		else {
 			try { console.log(data[0].complete_url); } catch (e){console.log(e)}
-			res.render('blankPage');			
+			if(blankPage == 1) res.render('blankPage');
+			if(blankPage == 0 ){
+				res.render('alertMain', {
+					name: req.body.name,
+					product: req.body.product
+				});
+				setTimeout(function(){ blankPage = 1 }, 7000);
+			}
 		}
-	});
-})
-
-router.get('/streamer_url', (req, res) => {
-	res.render('alertMain', {
-		name: req.body.name,
-		product: req.body.product
-	});
-})
+	})
+});
 
 router.post('/:streamer_url', (req, res) => {
-	res.redirect(`/${req.params.streamer_url}`)
+	
 	console.log(req.params.streamer_url)
+	blankPage = 0
 	res.status(200)
 })
 
